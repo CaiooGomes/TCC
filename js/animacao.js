ce51c1e2d8f58f02288aa50219069e51e9bcf947
CaiooGomes <gomescaio898@gmail.com>
2023-11-25 17:43:00 -0300

    Animação botao navbar

--- js/animacao.js
+++ js/animacao.js
@@ -1,18 +1,23 @@
-var elemento = document.getElementById("tresbarras");
-var barralateral = document.getElementById("navbar");
 
-elemento.addEventListener("mouseover", function() {
-    barralateral.classList.add("aumentartamanho")
-})
+var barralateral = document.getElementById("navbar");
+var botoes = document.getElementById("animacaoaumentadiminui")
+var botaonavegar = document.getElementById("botaonavegar")
+
 barralateral.addEventListener("mouseover", function() {
     barralateral.classList.remove("diminuirtamanho")
-})
-barralateral.addEventListener("mouseover", function() {
     barralateral.classList.add("aumentartamanho")
-})
-barralateral.addEventListener("mouseout", function() {
-    barralateral.classList.remove("aumentartamanho")
+    botaonavegar.innerHTML = ("<img src='../imagens/icons8-up-arrow-50.png'><h2>Oi bom dia</h2>")
 })
 barralateral.addEventListener("mouseout", function() {
     barralateral.classList.add("diminuirtamanho")
+    barralateral.classList.remove("aumentartamanho")
+    botaonavegar.innerHTML = ("<img src='../imagens/icons8-up-arrow-50.png'>")
+})
+botoes.addEventListener("mouseover", function() {
+    botaonavegar.classList.add("aumentarbotao")
+    botaonavegar.classList.remove("diminuirbotao")
+})
+botoes.addEventListener("mouseout", function() {
+    botaonavegar.classList.add("diminuirbotao")
+    botaonavegar.classList.remove("aumentarbotao")
 })
